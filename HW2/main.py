e5497169c8645a7f53beffcd917a386c914d7948
KJay221 <k20002216408@gmail.com>
2022-10-30 17:16:49 +0800

    add prediction

--- HW2/main.py
+++ HW2/main.py
@@ -17,9 +17,9 @@ from keras.callbacks import ReduceLROnPlateau
 
 class Window(QtWidgets.QWidget, UI.Ui_Form):
     classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-    demo = False
     learning_rate = 0.01
     epochs = 30
+
 
     def __init__(self):
         super(Window, self).__init__()
@@ -35,6 +35,10 @@ class Window(QtWidgets.QWidget, UI.Ui_Form):
         self.SMS.clicked.connect(self.FunctionSMS)
         self.Inference.clicked.connect(self.FunctionInference)
 
+        # combo box
+        self.Demo.addItem('Demo')
+        self.Demo.addItem('Not Demo')
+
         # load data
         (self.trainX, self.trainY), (self.testX, self.testY) = cifar10.load_data()
         self.x_train = self.trainX.astype('float32')/255
@@ -42,9 +46,46 @@ class Window(QtWidgets.QWidget, UI.Ui_Form):
         self.y_train = np_utils.to_categorical(self.trainY)
         self.y_test = np_utils.to_categorical(self.testY)
 
-        # reduce size
-        # self.x_train = self.x_train[0:1000]
-        # self.y_train = self.y_train[0:1000]
+        # get model
+        if self.Demo.currentText() == 'Not Demo':
+            VGG19 = tf.keras.applications.VGG19(
+                include_top = False, input_shape = (32, 32, 3), classes = 10)
+            self.model = tf.keras.models.Sequential()
+            self.model.add(VGG19)
+            self.model.add(Flatten())
+            self.model.add(Dense(1024,activation = 'relu'))
+            self.model.add(Dropout(.25))
+            self.model.add(Dense(1024,activation = 'relu'))
+            self.model.add(Dropout(.25))
+            self.model.add(Dense(256,activation = 'relu'))
+            self.model.add(Dense(10,activation = 'softmax'))
+
+            optimizer = optimizers.SGD(learning_rate = self.learning_rate, decay=1e-6, momentum=0.9, nesterov=True)
+            self.model.compile(optimizer = optimizer, loss = 'categorical_crossentropy', metrics = ['accuracy'])
+            learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', 
+                patience=3, verbose=1, factor=0.6, min_lr=0.00001)
+            result = self.model.fit(
+                x = self.x_train, y = self.y_train, epochs = self.epochs, batch_size = 32, validation_split=0.15, verbose = 1, callbacks = [learning_rate_reduction])
+            self.model.save('./model/model.h5')
+            plt.plot(result.history['accuracy'])
+            plt.plot(result.history['val_accuracy'])
+            plt.title('model accuracy')
+            plt.ylabel('accuracy')
+            plt.xlabel('epoch')
+            plt.legend(['train', 'test'], loc='upper left') 
+            plt.savefig('./pic/accuracy.png')   
+            plt.close()
+
+            plt.plot(result.history['loss']) 
+            plt.plot(result.history['val_loss']) 
+            plt.title('model loss')
+            plt.ylabel('loss')
+            plt.xlabel('epoch')
+            plt.legend(['train', 'test'], loc='upper left') 
+            plt.savefig('./pic/loss.png')
+            plt.close()
+        else:
+            self.model = keras.models.load_model('./model/model.h5')
 
 
     def FunctionLoadImage1(self):
@@ -103,9 +144,9 @@ class Window(QtWidgets.QWidget, UI.Ui_Form):
         cv2.imshow("img", showimg)
 
     def FunctionLoadImage(self):
-        number = random.randint(0, 9999)
-        img_path = './pic/' + str(number) + '.png'
-        cv2.imwrite(img_path, self.testX[number])
+        self.test_number = random.randint(0, 9999)
+        img_path = './pic/' + str(self.test_number) + '.png'
+        cv2.imwrite(img_path, self.testX[self.test_number])
         pix = QPixmap(img_path)
         pix = pix.scaled(315,315)
         item = QtWidgets.QGraphicsPixmapItem(pix)
@@ -114,7 +155,7 @@ class Window(QtWidgets.QWidget, UI.Ui_Form):
         scene.addItem(item)
         self.Image.setScene(scene)
         os.remove(img_path)
-        self.Label.setText('Label = ' + self.classes[self.testY[number][0]])
+        self.Label.setText('Label = ' + self.classes[self.testY[self.test_number][0]])
         
     def FunctionSTI(self):
         number = random.randint(0, 49991)
@@ -127,65 +168,19 @@ class Window(QtWidgets.QWidget, UI.Ui_Form):
         plt.show()
 
     def FunctionSMS(self):
-        if self.demo == False:
-            VGG19 = tf.keras.applications.VGG19(
-                include_top = False, input_shape = (32, 32, 3), classes = 10)
-            self.model = tf.keras.models.Sequential()
-            self.model.add(VGG19)
-            self.model.add(Flatten())
-            self.model.add(Dense(1024,activation = 'relu'))
-            self.model.add(Dropout(.25))
-            self.model.add(Dense(1024,activation = 'relu'))
-            self.model.add(Dropout(.25))
-            self.model.add(Dense(256,activation = 'relu'))
-            self.model.add(Dense(10,activation = 'softmax'))
-
-            optimizer = optimizers.SGD(learning_rate = self.learning_rate, decay=1e-6, momentum=0.9, nesterov=True)
-            self.model.compile(optimizer = optimizer, loss = 'categorical_crossentropy', metrics = ['accuracy'])
-            learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', 
-                patience=3, verbose=1, factor=0.6, min_lr=0.00001)
-            result = self.model.fit(
-                x = self.x_train, y = self.y_train, epochs = self.epochs, batch_size = 32, validation_split=0.15, verbose = 1, callbacks = [learning_rate_reduction])
-            self.model.save('./model/model.h5')
-            plt.plot(result.history['accuracy'])
-            plt.plot(result.history['val_accuracy'])
-            plt.title('model accuracy')
-            plt.ylabel('accuracy')
-            plt.xlabel('epoch')
-            plt.legend(['train', 'test'], loc='upper left') 
-            plt.savefig('./pic/accuracy.png')   
-            plt.close()
-
-            plt.plot(result.history['loss']) 
-            plt.plot(result.history['val_loss']) 
-            plt.title('model loss')
-            plt.ylabel('loss')
-            plt.xlabel('epoch')
-            plt.legend(['train', 'test'], loc='upper left') 
-            plt.savefig('./pic/loss.png')
-            plt.close()
-        else:
-            self.model = keras.models.load_model('./model/model.h5')
         print(self.model.summary())
 
     def FunctionInference(self):
-        img = np.array([self.x_test[5]])
+        img = np.array([self.x_test[self.test_number]])
         tag = []
         for i in range(10):
             tag.append(self.classes[i])
-        tag[0] = 'plane'
-        tag[1] = 'car'
-        pred = self.model.predict(img)
-        #print(pred[0])
+        prediction = self.model.predict(img)
+        self.PredictionLabel.setText('Prediction Label = ' + self.classes[prediction.argmax()])
+        self.Confidence.setText('Confidence = ' + "{:.4f}".format(prediction[0][prediction.argmax()]))
 
-        fig = plt.figure()
-        fig.set_size_inches(12, 6)
-        plt.subplot(1, 2, 1)
-        plt.imshow(img[0])
-        plt.axis('off')
-        plt.subplot(1, 2, 2)
-        plt.bar(tag, pred[0])
-        plt.show()       
+        print()
+
 
         
 
