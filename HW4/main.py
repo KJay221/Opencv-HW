8e84765f6af492255f92df7697da6f1e33c97732
KJay221 <k20002216408@gmail.com>
2022-12-04 11:56:26 +0800

    add HW4 UI

---
+++ HW4/main.py
@@ -0,0 +1,18 @@
+import sys
+import UI
+from PyQt5 import QtWidgets
+
+
+class Window(QtWidgets.QWidget, UI.Ui_Form):
+    
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setupUi(self)
+
+        
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    ui = Window()
+    ui.show()
+    sys.exit(app.exec_())
